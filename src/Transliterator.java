ba4a6a4fda82a5adb2945f7549a3979b8558c3fe
Ivanova Darya <dashana2112@gmail.com>
2020-06-20 23:16:10 +0800

    Отформатировано

--- src/Transliterator.java
+++ src/Transliterator.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 
 public class Transliterator {
 
-    public static final char[] rus =  new char[]{'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+    public static final char[] rus = new char[]{'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
     public static final String[] eng = new String[]{"a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "i", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "ie", "y", "", "e", "iu", "ia"};
 
     public static Transliterator createICAO_DOC_9303() {
@@ -23,12 +23,16 @@ public class Transliterator {
         int control = -1;
         for (int i = 0; i < given.length; i++) {
             if (Character.toLowerCase(c) == given[i]) {
-               control = i;
+                control = i;
             }
         }
-        if (control == -1) { return String.valueOf(c); }
-        else if (Character.isUpperCase(c)) { return register(become[control]); }
-        else { return become[control]; }
+        if (control == -1) {
+            return String.valueOf(c);
+        } else if (Character.isUpperCase(c)) {
+            return register(become[control]);
+        } else {
+            return become[control];
+        }
     }
 
     public String translate(String s) {
@@ -39,7 +43,7 @@ public class Transliterator {
         return res.toString();
     }
 
-    public void translateFile(String first, String second) throws IOException  {
+    public void translateFile(String first, String second) throws IOException {
         String s = Files.readString(Paths.get(first));
         s = translate(s);
         byte[] strToBytes = s.getBytes();
@@ -47,7 +51,10 @@ public class Transliterator {
     }
 
     private String register(String s) {
-        if (s.length() < 1) { return s; }
-        else { return s.substring(0, 1).toUpperCase() + s.substring(1); }
+        if (s.length() < 1) {
+            return s;
+        } else {
+            return s.substring(0, 1).toUpperCase() + s.substring(1);
+        }
     }
 }
